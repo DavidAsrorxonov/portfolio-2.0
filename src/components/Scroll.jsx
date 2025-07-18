4c3cd2938b530f24a43128af2a674b944089e72e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-07-18 16:44:43 +0900

    Add Scroll component with GSAP animation and Lottie integration; update package.json and package-lock.json

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,27 @@
+import Lottie from "lottie-react";
+import ScrollAnim from "../assets/scroll.json";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+const Scroll = () => {
+  const ScrollAnimRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      ScrollAnimRef.current,
+      { opacity: 0, scale: 0, y: 500 },
+      { opacity: 1, scale: 1, y: 0, duration: 4, delay: 3 }
+    );
+  });
+
+  return (
+    <div
+      className="flex items-center justify-center w-full mt-20"
+      ref={ScrollAnimRef}
+    >
+      <Lottie animationData={ScrollAnim} loop={true} />
+    </div>
+  );
+};
+
+export default Scroll;
--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Intro from "../components/Intro";
+import Scroll from "../components/Scroll";
 
 const Landing = () => {
   return (
     <div>
       <Header />
       <Intro />
+      <Scroll />
     </div>
   );
 };
