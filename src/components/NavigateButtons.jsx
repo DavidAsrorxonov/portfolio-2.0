441ea6ab45db6db85dda456883a7d8d341253b41
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-07-20 18:47:07 +0900

    feat: implement NavigateButtons component with GSAP animations in About section

--- src/components/NavigateButtons.jsx
+++ src/components/NavigateButtons.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const NavigateButtons = () => {
-  return <div>NavigateButtons</div>;
+  const navigateButtonsRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      navigateButtonsRef.current,
+      { opacity: 0, scale: 0, x: 0 },
+      {
+        opacity: 1,
+        scale: 1,
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: navigateButtonsRef.current,
+          start: "top bottom",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <div className="absolute bottom-6 right-10 z-50" ref={navigateButtonsRef}>
+      <div className="flex gap-3">
+        <button className="flex items-center justify-center h-12 w-12 rounded-full bg-[#333335] hover:bg-[#444446] transition-colors">
+          <ChevronLeft size={30} strokeWidth={3} className="text-white" />
+        </button>
+
+        <button className="flex items-center justify-center h-12 w-12 rounded-full bg-[#333335] hover:bg-[#444446] transition-colors">
+          <ChevronRight size={30} strokeWidth={3} className="text-white" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NavigateButtons;
--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SectionTitle from "../components/SectionTitle";
 import CardContainer from "../components/CardContainer";
+import NavigateButtons from "../components/NavigateButtons";
 
 const About = () => {
   return (
-    <div className="h-screen bg-[#161616]">
+    <div className="relative h-screen bg-[#161616]">
       <SectionTitle title="Get to know me" />
       <CardContainer />
+      <NavigateButtons />
     </div>
   );
 };
