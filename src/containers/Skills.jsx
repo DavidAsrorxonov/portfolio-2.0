0718d5f897502aa488d90b908b0156719096ada5
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-07-22 15:28:34 +0900

    feat: refactor skills and icons management; remove hardcoded skills and implement Icons module; Add Icons Aggregator

--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -6,13 +6,6 @@ const Skills = () => {
   return (
     <div className="h-screen">
       <SectionTitle title="What I know" />
-
-      {skills.map(({ id, skill, Icon }) => (
-        <div key={id}>
-          <h1 className="text-white">{skill}</h1>
-          <Icon />
-        </div>
-      ))}
     </div>
   );
 };
---
+++ src/lib/Icons.js
@@ -0,0 +1,15 @@
+import * as Langs from "../../public/assets/langs";
+import * as Frontend from "../../public/assets/frontend";
+import * as Backend from "../../public/assets/backend";
+import * as Tools from "../../public/assets/tools";
+import * as DB from "../../public/assets/db";
+
+const Icons = {
+  Langs,
+  Frontend,
+  Backend,
+  Tools,
+  DB,
+};
+
+export default Icons;
